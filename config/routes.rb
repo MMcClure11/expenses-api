a56ee40cea4caad0f5220250b6c8627f8bc1ef81
MMcClure11 <mcclme02@luther.edu>
2020-11-18 09:01:05 -0600

    adds namespacing to routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  resources :transactions
-  resources :accounts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :transactions
+      resources :accounts
+    end
+  end 
+  
 end
+
+# fetch(`http://localhost:3000/api/v1/transactions`)
+# => this is what we want, so users know it's an API and on v1
+# do this with namespacing
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
