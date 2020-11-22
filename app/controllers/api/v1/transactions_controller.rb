d135da96cf3c3aba969710bfbbb489596ae829f9
MMcClure11 <mcclme02@luther.edu>
2020-11-22 15:57:39 -0600

    adds actions to transactions controller

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::TransactionsController < ApplicationController
+
+  before_action :set_account
+
+  def index
+    @transactions = Transaction.all
+    render json: @transactions
+  end
+
+  def show
+    @transaction = Transaction.find(params[:id])
+    render json: @transaction
+  end
+
+  def create
+    @transaction = @account.transactions.new(transaction_params)
+
+    @transaction.date = DateTime.now
+    if @account.update_balance(@transaction) != 'Balance too low.'
+      @transaction.save
+      render json: @account
+    else
+      render json: {error: 'Balance too low'}
+    end
+  end
+
+  private
+
+  def set_account
+    @account = Account.find(params[:account_id])
+  end
+
+
+  def transaction_params
+    params.require(:transaction).permit(:amount, :account_id, :kind, :date, :description)
+  end
+
 end
